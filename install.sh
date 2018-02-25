fe1460c4215d5cf438747e4295b7f07b8b7f6963
kmille <christian.schneider@androidloves.me>
2018-02-25 16:26:19 +0100

    Update install script: use gloabl paths

--- install.sh
+++ install.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
-cp .zshrc ~/.zshrc
-cp .bashrc ~/.bashrc
-cp .tmux.conf ~/.tmux.conf
-cp .vimrc ~/.vimrc
-cp config ~/.config/i3/config
-cp .i3status.conf ~/.i3status.conf
-cp .gitconfig ~/.gitconfig
+# server
+mv bashrc ~/.bashrc
+mv bash_aliases ~/.bash_aliases
+mv tmux.conf ~/.tmux.conf
+mv vimrc /etc/vim/vimrc
+mv gitconfig /etc/gitconfig
+
+# clients only
+#mv zshrc ~/.zshrc
+#mv i3status.conf ~/.i3status.conf
+#mv config ~/.config/i3/config
