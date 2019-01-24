f7cf853083b415b89f7bd45925b3c8a8454fe8ca
kmille <christian.schneider@androidloves.me>
2019-01-24 14:26:56 +0100

    Fix double coding line

--- install.sh
+++ install.sh
@@ -10,10 +10,12 @@ cp bash_aliases ~/.bash_aliases
 # client
 cp zshrc ~/.zshrc
 cp config ~/.config/i3/config
+
 cp i3status.conf ~/.i3status.conf
 mkdir -p ~/.i3/py3status/
+pacman -S py3status
+pip install --user pyvbox
 cp vms.py ~/.i3/py3status/vms.py
-#pacman -S py3status
 i3-msg restart
 
 curl -sfLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
