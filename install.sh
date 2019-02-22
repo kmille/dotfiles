4b88e29fcb570fef3f76de212c1a4d45abd64281
kmille <christian.schneider@androidloves.me>
2019-02-22 12:46:00 +0100

    test bash vs zsh

--- install.sh
+++ install.sh
@@ -1,41 +1,28 @@
-#!/bin/sh
+#!/bin/bash
 
-case $SHELL in
-*/zsh) 
-        do_for_all
-        install_zsh
-   ;;
-*/bash)
-        do_for_all
-        install_bash
-   ;;
-*)
-   echo shell not found
-esac
-
-
+# do for all
 function do_for_all {
-cp tmux.conf ~/.tmux.conf
-curl -sfLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-cp vimrc ~/.vimrc
-cp gitconfig ~/.gitconfig
+    cp tmux.conf ~/.tmux.conf
+    curl -sfLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+    cp vimrc ~/.vimrc
+    cp gitconfig ~/.gitconfig
 }
 
 
 function install_bash {
-# server
-cp bashrc ~/.bashrc
-cp bash_aliases ~/.bash_aliases
+    # server
+    cp bashrc ~/.bashrc
+    cp bash_aliases ~/.bash_aliases
 
-## install rg
-wget https://github.com/BurntSushi/ripgrep/releases/download/0.10.0/ripgrep_0.10.0_amd64.deb
-dpkg -i ripgrep_0.10.0_amd64.deb
-rm ripgrep_0.10.0_amd64.deb
+    ## install rg
+    wget https://github.com/BurntSushi/ripgrep/releases/download/0.10.0/ripgrep_0.10.0_amd64.deb
+    dpkg -i ripgrep_0.10.0_amd64.deb
+    rm ripgrep_0.10.0_amd64.deb
 
-## install fd
-wget https://github.com/sharkdp/fd/releases/download/v7.2.0/fd-musl_7.2.0_amd64.deb
-dpkg -i fd-musl_7.2.0_amd64.deb
-rm fd-musl_7.2.0_amd64.deb
+    ## install fd
+    wget https://github.com/sharkdp/fd/releases/download/v7.2.0/fd-musl_7.2.0_amd64.deb
+    dpkg -i fd-musl_7.2.0_amd64.deb
+    rm fd-musl_7.2.0_amd64.deb
 }
 
 
@@ -51,3 +38,16 @@ function install_zsh {
     cp vms.py ~/.i3/py3status/vms.py
     i3-msg restart
 }
+
+
+if $(which zsh) 
+then
+    #do_for_all
+    #install_zsh
+    echo 1
+else
+    #do_for_all
+    #install_bash
+    echo 2
+
+fi
